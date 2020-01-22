80cdbdce06071e79905a12765d6e0cdf4d0e0be5
Fedor Metelkin <f.metelkin@ardocs.ru>
2020-01-22 21:43:49 +0300

    SpecialState DB script added.

--- apricom-web/src/main/java/ru/apricom/testapp/entities/person/SpecialState.java
+++ apricom-web/src/main/java/ru/apricom/testapp/entities/person/SpecialState.java
@@ -1,28 +1,65 @@
 package ru.apricom.testapp.entities.person;
 
+import ru.apricom.testapp.entities.base.Speciality;
+import ru.apricom.testapp.entities.documents.OtherDocument;
+import ru.apricom.testapp.entities.entrant.Entrant;
 import ru.apricom.testapp.entities.entrant.EntrantStatus;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "special_state")
-public class SpecialState {
+public class SpecialState implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     private long id;
 
-    @Column(name = "file_id")
-    private String file_id;
+    @ManyToOne
+    @JoinColumn(name = "document_id")
+    private OtherDocument document;
 
-    //@ManyToOne(cascade = CascadeType.ALL)
-    //@JoinColumn(name = "person_id")
+    @ManyToOne
+    @JoinColumn(name = "entrant_id")
     @NotNull
-    private PersonName name;
+    private Entrant entrant;
 
-    @Column(name = "speciality")
-    //@ManyToOne(cascade = CascadeType.ALL)
-    //@JoinColumn(name = "speciality_id")
-    private String speciality;
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public OtherDocument getDocument() {
+        return document;
+    }
+
+    public void setDocument(OtherDocument document) {
+        this.document = document;
+    }
+
+    public Entrant getEntrant() {
+        return entrant;
+    }
+
+    public void setEntrant(Entrant entrant) {
+        this.entrant = entrant;
+    }
+
+    public Speciality getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(Speciality speciality) {
+        this.speciality = speciality;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "speciality_id")
+    private Speciality speciality;
 
 }
